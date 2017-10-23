fb2d0381337752a2cb5466e98f90f7ac62eb0f10
adrianmarino <adrianmarino@gmail.com>
2017-10-22 22:34:10 -0300

    first commit

---
+++ Assets/Player/Scripts/ChangeCamera.js
@@ -0,0 +1,18 @@
+﻿var cam : Camera;
+var cam1 : Camera;
+ 
+function Start () {
+    cam.enabled=true;
+    cam1.enabled=false;
+}
+ 
+function Update () {
+    if(Input.GetKeyDown("1")) {
+        cam.enabled=true;
+        cam1.enabled=false;
+    } 
+    if(Input.GetKeyDown("2")) {
+        cam.enabled=false;
+        cam1.enabled=true;
+    }
+}
